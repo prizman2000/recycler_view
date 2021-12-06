6a7ddcd560b0267df9989e2f1164c04ff0eb9ac2
neu_net <przman2000@mail.ru>
2021-12-06 23:55:54 +1000

    created recycler view example

---
+++ app/src/main/java/com/example/recyclerview/CustomAdapter.kt
@@ -0,0 +1,37 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.ItemsViewModel
+import com.example.recyclerview.R
+
+class CustomAdapter(private val mList: List<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_design, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val ItemsViewModel = mList[position]
+
+        holder.imageView.setImageResource(ItemsViewModel.image)
+        holder.titleView.text = ItemsViewModel.title
+        holder.textView.text = ItemsViewModel.text
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageview)
+        val titleView: TextView = itemView.findViewById(R.id.titleView)
+        val textView: TextView = itemView.findViewById(R.id.textView)
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/ItemsViewModel.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerview
+
+data class ItemsViewModel(val image: Int, val title: String, val text: String) {}
